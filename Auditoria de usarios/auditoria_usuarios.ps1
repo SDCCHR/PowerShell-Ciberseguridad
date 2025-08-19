7ff74bc63eb4d43dc806813a2fb0175193f7215c
SDCCHR <sofia.chavezre@uanl.edu.mx>
2025-08-19 09:39:30 -0600

    Add files via upload

---
+++ Auditoria de usarios/auditoria_usuarios.ps1
@@ -0,0 +1,20 @@
+﻿$usuarios = get-LocalUser
+$sinlogon = @()
+$conlogon = @()
+foreach ($u in $usuarios){
+    if (-not $u.lastLogon){
+        $sinlogon +="$($u.Name): Estado = $($u.Enabled), Ultimo acceso = NUNCA"
+    } else{
+          $conlogon += "$($u.Name): Estado = $($u.Enabled), Ultimo acceso = $($u.lastlogon)"
+    }
+}
+#para guardar en archivos separados 
+
+$sinlogon | Out-File -FilePath "$env:USERPROFILE\Desktop\usuarios_sin_logon.txt"
+$conlogon | Out-File -FilePath "$env:USERPROFILE\Desktop\usarios_con_logon.txt"
+
+#para mostrarlo en pantalla
+Write-Output "`n Usuarios que NUNCA han iniciado sesion: "
+$sinlogon | ForEach-Object { Write-Output $_}
+Write-Output "`n Usuarios que SIan iniciado sesion: "
+$conlogon | ForEach-Object { Write-Output $_ }
---
+++ ejercicio 5/ManejoDeErrores.ps1
@@ -0,0 +1,20 @@
+﻿function Validar-Archivo{
+    param([string]$Ruta)
+
+    try{
+        if(Test-Path $Ruta){
+            $contenido = Get-Content $Ruta -ErrorAction Stop
+            return "archivo encontrado y accesible: $Ruta"
+        }else{
+            throw "el archivo no existe"
+        }
+    }
+    catch{
+        return "error: $_"
+    }
+    finally{
+        Write-Host "validacion finalizada para: $Ruta" -ForegroundColor Cyan
+    }
+}
+Validar-Archivo -Ruta "C:\archivo_inexistente.txt"
+Validar-Archivo -Ruta "$env:USERPROFILE\Desktop\archivo.txt"
